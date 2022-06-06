94a841affc30956d10a69d1cc28fa5de8da52c0b
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2022-06-06 11:06:59 +0200

    add scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import style from  './app.module.scss';
+import "./utils/_variables.scss";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className={style.App}>
+      <span>Test</span>
     </div>
   );
 }
