c7f60a1bb297e48f5772c4e682e7a0030f78fbb7
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2022-07-11 18:16:46 +0200

    Add SnackBar at Save

--- src/components/buttons/SaveButton.jsx
+++ src/components/buttons/SaveButton.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './saveButton.module.scss'
 import apiFirebase from '../../conf/api.firebase';
 import { useSelector } from 'react-redux/es/exports';
-
-
+import SnackBarSave from '../snackBar/snackBarSave';
 
 const SaveButton = () => {
-
+  const [isSaved, setSave] = useState(false);
   const lists = useSelector(state => state.lists)
   const saveTodos = ()=>{
     apiFirebase.put('todos.json', lists)
+    .then(setSave(true))
   }
   return (
+    <>
     <button type='button' onClick={()=>saveTodos()} className={style.btnSave}>
       <img src="./svg/save.svg" alt='save'/>
     </button>
+    {isSaved ? <SnackBarSave/> : ""}
+    </>
   )
 }
 
---
+++ src/components/snackBar/snackBarSave.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function SnackBarSave() {
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(()=>{
+    setOpen(true);
+  },[])
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+            Vos listes ont été sauvegardé !
+        </Alert>
+      </Snackbar>
+  );
+}
