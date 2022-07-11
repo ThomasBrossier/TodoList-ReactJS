f55ee4c02e39cd03da204fad0676416e587408dd
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2022-07-11 18:44:18 +0200

    Change save button for snackBar

--- src/components/buttons/saveButton.jsx
+++ src/components/buttons/saveButton.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import style from './saveButton.module.scss';
+import React, { useState } from 'react'
+import style from './saveButton.module.scss'
 import apiFirebase from '../../conf/api.firebase';
+import { useSelector } from 'react-redux/es/exports';
+import SnackBarSave from '../snackBar/snackBarSave';
 
-const saveButton = () => {
-
-   const saveFavoris = ()=>{
-        apiFirebase.put('TodoLists.json', this.state.favoris)
-      }
+const SaveButton = () => {
+  const [isSaved, setSave] = useState(false);
+  const lists = useSelector(state => state.lists)
+  const saveTodos = ()=>{
+    apiFirebase.put('todos.json', lists)
+    .then(setSave(true))
+  }
   return (
-    <button className={style.btnSave} onClick={()=>saveFavoris()}></button>
+    <>
+    <button type='button' onClick={()=>saveTodos()} className={style.btnSave}>
+      <img src="./svg/save.svg" alt='save'/>
+    </button>
+    {isSaved ? <SnackBarSave/> : ""}
+    </>
   )
 }
 
-export default saveButton
+export default SaveButton
